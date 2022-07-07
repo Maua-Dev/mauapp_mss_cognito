b0cbebd4410823614794fa50fb9d4c189ded46d8
goodguyplayer <nathanbritodasilva@gmail.com>
2022-07-06 21:50:12 -0300

    TEST - test_user fix

--- test/domain/entities/test_user.py
+++ test/domain/entities/test_user.py
@@ -12,99 +12,105 @@ class Test_User():
 
     def test_create_valid_user(self):
         user = User(id="123", name='Bruno Vilardi', ra=19003315, year=YEAR_ENUM._4,
-                 course=DegreeEnum.ECM, image="www.link.com.br", email="bruno@bruno.com"
+                 course=DegreeEnum.ECM, email="bruno@bruno.com"
                 )
 
-        assert user.name == 'Joao Do Teste'
+        assert user.name == 'Bruno Vilardi'
         assert user.ra == '19003315'
         assert user.year == YEAR_ENUM._4
         assert user.course.value == 'Engenharia de Computação'
-        assert user.image == 'www.link.com.br'
+        assert user.email == 'bruno@bruno.com'
 
     def test_create_valid_user1(self):
         user = User(name='Joao do Teste', ra='12345678',
-                    year=2000, course='Engenharia de algo',
-                    image='www.google.com'
+                    year=YEAR_ENUM._1, course='Engenharia Fundamentos Básicos',
+                    email='www.google.com'
                     )
 
         assert len(user.name) > 0
         assert user.name == 'Joao Do Teste'
         assert len(user.ra) == 8
         assert user.ra == '12345678'
-        assert user.year == 2000
-        assert user.year > 0
-        assert len(user.course) > 0
-        assert user.course == 'Engenharia de algo'
-        assert user.image == 'www.google.com'
+        assert user.year.value == 1
+        assert len(user.course.value) > 0
+        assert user.course.value == 'Engenharia Fundamentos Básicos'
+        assert user.email == 'www.google.com'
 
     def test_create_valid_user2(self):
-        user = User(name='joao do teste', ra='12345678',
-                    year=2000, course='Engenharia de algo',
-                    image='www.google.com'
-                )
+        user = User(name='Joao do Teste', ra='87654321',
+                    year=YEAR_ENUM._6, course='Ensino a Distância',
+                    email='www.google.com', password="12345679"
+                    )
 
         assert len(user.name) > 0
         assert user.name == 'Joao Do Teste'
-        assert user.ra == '12345678'
-        assert user.year == 2000
-        assert user.year > 0
-        assert len(user.course) > 0
-        assert user.course == 'Engenharia de algo'
-        assert user.image == 'www.google.com'
+        assert user.ra == '87654321'
+        assert user.year == YEAR_ENUM._6
+        assert user.year.value > 0
+        assert len(user.course.value) > 0
+        assert user.course == DegreeEnum.EAD
+        assert user.email == 'www.google.com'
+        assert user.password == '12345679'
+        assert len(user.password) >= 8
 
 
     def test_create_invalid_user1(self):
-        with pytest.raises(EntityError):
-            User(name='', ra='12345678',
-                    year=2000, course='Engenharia de algo',
-                    image='www.google.com'
+        with pytest.raises((EntityError, ValidationError)):
+            User(ra='87654321',
+                 year=YEAR_ENUM._6, course='Ensino a Distância',
+                 email='www.google.com', password="12345679"
                  )
 
     def test_create_invalid_user2(self):
-        with pytest.raises(EntityError):
-            User(name='Joao do Teste', ra='1234567',
-                    year=2000, course='Engenharia de algo',
-                    image='www.google.com'
+        with pytest.raises((EntityError, ValidationError)):
+            User(name='Joao do Teste', ra='87654321',
+                 year=2002, course='Ensino a Distância',
+                 email='www.google.com', password="12345679"
                  )
 
     def test_create_invalid_user3(self):
-        with pytest.raises(EntityError):
-            User(name='Joao do Teste', ra='12345678',
-                    year=1999, course='Engenharia de algo',
-                    image='www.google.com'
+        with pytest.raises((EntityError, ValidationError)):
+            User(name='Joao do Teste', ra='87654321',
+                 year=YEAR_ENUM._3, course='Ensino a Distância',
                  )
 
     def test_create_invalid_user4(self):
-        with pytest.raises(EntityError):
-            User(name='Joao do Teste', ra='12345678',
-                    year=2000, course='',
-                    image='www.google.com'
+        with pytest.raises((EntityError, ValidationError)):
+            User(name='Joao do Teste', ra='123456789101112',
+                 year=YEAR_ENUM._6, course='Ensino a Distância',
+                 email='www.google.com', password="12345679"
                  )
 
     def test_create_invalid_user5(self):
         with pytest.raises((EntityError, ValidationError)):
-            User(name='Joao do Teste', ra='12345678',
-                    year=2000, course='Engenharia de algo',
-                    image=''
+            User(name='Joao do Teste',
+                 year=YEAR_ENUM._6, course='Ensino a Distância',
+                 email='www.google.com', password="12345679"
                  )
 
     def test_create_invalid_user6(self):
         with pytest.raises((EntityError, ValidationError)):
-            User(name='Joao do Teste', ra='12345678',
-                    year=-1922, course='Engenharia de algo',
-                    image='www.google.com'
+            User(name='Joao do Teste', ra='87654321',
+                 year=1984, course='Ensino a Distância',
+                 email='www.google.com', password="12345679"
                  )
 
     def test_create_invalid_user7(self):
         with pytest.raises((EntityError, ValidationError)):
             User(name='Joao do Teste', ra='123456789',
-                 year=2000, course='Engenharia de algo',
+                 year=YEAR_ENUM._3.value, course='Ensino a Distância',
                  image='www.google.com'
                  )
 
     def test_create_invalid_user8(self):
         with pytest.raises((EntityError, ValidationError)):
-            User(name='Joao do Teste', ra='',
-                    year=2000, course='Engenharia de algo',
-                    image='www.google.com'
-                    )
+            User(name='Joao do Teste', ra='123456789',
+                 year=YEAR_ENUM._3.value, course=DegreeEnum.ECM,
+                 image='www.google.com'
+                 )
+
+    def test_create_invalid_user9(self):
+        with pytest.raises((EntityError, ValidationError)):
+            User(name='Joao do Teste', ra='123456789',
+                 year=YEAR_ENUM._3.value, image='www.google.com'
+                 )
